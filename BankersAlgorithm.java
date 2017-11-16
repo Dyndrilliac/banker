0fe8373d68d8625aed2e18251756bc157fc83dc7
Matthew Boyette <Dyndrilliac@gmail.com>
2017-11-15 22:09:18 -0500

    Initial code upload

---
+++ BankersAlgorithm.java
@@ -0,0 +1,363 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import Jama.Matrix;
+
+public class BankersAlgorithm
+{
+    protected static final class RequestData
+    {
+        private int        process;
+        private double[][] request;
+
+        public RequestData(final int process, final double[][] request)
+        {
+            super();
+            this.setProcess(process);
+            this.setRequest(request);
+        }
+
+        public final int getProcess()
+        {
+            return process;
+        }
+
+        public final double[][] getRequest()
+        {
+            return request;
+        }
+
+        protected final void setProcess(final int process)
+        {
+            this.process = process;
+        }
+
+        protected final void setRequest(final double[][] request)
+        {
+            this.request = request;
+        }
+    }
+
+    public static final BankersAlgorithm inputInitialData(final Scanner input) throws InputMismatchException, NoSuchElementException
+    {
+        int n = 0, m = 0;
+        double[][] available = null, allocation = null, max = null;
+
+        System.out.println("Please enter the number of processes.");
+        n = input.nextInt();
+
+        System.out.println("\nPlease enter the number of resource types.");
+        m = input.nextInt();
+
+        System.out.println("\nPlease enter the available vector.");
+        available = new double[1][m];
+
+        for ( int j = 0; j < m; j++ )
+        {
+            available[0][j] = input.nextDouble();
+        }
+
+        System.out.println("\nPlease enter the max matrix.");
+        max = new double[n][m];
+
+        for ( int i = 0; i < n; i++ )
+        {
+            for ( int j = 0; j < m; j++ )
+            {
+                max[i][j] = input.nextDouble();
+            }
+        }
+
+        System.out.println("\nPlease enter the allocation matrix.");
+        allocation = new double[n][m];
+
+        for ( int i = 0; i < n; i++ )
+        {
+            for ( int j = 0; j < m; j++ )
+            {
+                allocation[i][j] = input.nextDouble();
+            }
+        }
+
+        System.out.println();
+        return new BankersAlgorithm(n, m, new Matrix(available), new Matrix(max), new Matrix(allocation));
+    }
+
+    public static final RequestData inputRequestData(final Scanner input, final int m) throws InputMismatchException, NoSuchElementException
+    {
+        int process = 0;
+        double[][] request = null;
+
+        System.out.println("\nWhich process is requesting increased resources?");
+        process = input.nextInt();
+
+        System.out.println("\nPlease enter the request vector.");
+        request = new double[1][m];
+
+        for ( int j = 0; j < m; j++ )
+        {
+            request[0][j] = input.nextDouble();
+        }
+
+        System.out.println();
+        return new RequestData(process, request);
+    }
+
+    public static final void main(final String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        try
+        {
+            BankersAlgorithm ba = BankersAlgorithm.inputInitialData(input);
+
+            if ( ba.isSafe() )
+            {
+                System.out.println("Safe State!");
+
+                RequestData rd = BankersAlgorithm.inputRequestData(input, ba.getM());
+
+                if ( ba.request(rd.getProcess(), new Matrix(rd.getRequest())) )
+                {
+                    System.out.println("Request granted: Safe State!");
+                }
+                else
+                {
+                    System.out.println("Request denied: Unsafe State!");
+                }
+            }
+            else
+            {
+                System.out.println("Unsafe State!");
+            }
+        }
+        catch ( final Exception e )
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        finally
+        {
+            input.close();
+        }
+    }
+
+    /*
+     * Let n be the number of processes.
+     * Let m be the number of resource types.
+     * 
+     * Let available be a vector of length m.
+     * If available[j] = k, then there are k instances of resource type R_j available.
+     * 
+     * Let max be a matrix of dimensions n x m.
+     * If max[i,j] = k, then process P_i may request at most k instances of resource type R_j.
+     * 
+     * Let allocation be a matrix of dimensions n x m.
+     * If allocation[i,j] = k, then process P_i is currently allocated k instances of R_j.
+     * 
+     * Let need be a matrix of dimensions n x m.
+     * If need[i,j] = k, then process P_i may need k more instances of R_j to complete its task.
+     * 
+     * Note that need[i,j] = max[i,j] – allocation[i,j].
+     */
+
+    private Matrix allocation = null;
+    private Matrix available  = null;
+    private int    m          = 0;
+    private Matrix max        = null;
+    private int    n          = 0;
+
+    public BankersAlgorithm(final int n, final int m, final Matrix available, final Matrix max, final Matrix allocation)
+    {
+        super();
+        this.setN(n);
+        this.setM(m);
+        this.setAvailable(available);
+        this.setMax(max);
+        this.setAllocation(allocation);
+    }
+
+    protected final boolean check(final int i1, final int i2, final Matrix A, final Matrix B)
+    {
+        for ( int j = 0; j < this.getM(); j++ )
+        {
+            if ( A.getArray()[i1][j] < B.getArray()[i2][j] ) { return false; }
+        }
+
+        return true;
+    }
+
+    public final BankersAlgorithm copy()
+    {
+        // Creates a deep copy of this BankersAlgorithm object.
+        return new BankersAlgorithm(this.getN(), this.getM(), this.getAvailable().copy(), this.getMax().copy(), this.getAllocation().copy());
+    }
+
+    public final Matrix getAllocation()
+    {
+        return this.allocation;
+    }
+
+    public final Matrix getAvailable()
+    {
+        return this.available;
+    }
+
+    public final int getM()
+    {
+        return this.m;
+    }
+
+    public final Matrix getMax()
+    {
+        return this.max;
+    }
+
+    public final int getN()
+    {
+        return this.n;
+    }
+
+    public final Matrix getNeed()
+    {
+        return this.getMax().minus(this.getAllocation());
+    }
+
+    public final boolean isSafe()
+    {
+        /*
+         * 1. Let work and finish be vectors of length m and n, respectively.
+         *    work = available.
+         *    finish[i] = false for i from 0 to n-1.
+         * 
+         * 2. Find a value for i such that both of the following conditions are true:
+         *    (a) finish[i] = false.
+         *    (b) need_i <= work.
+         *    If no such i exists, then goto step 4.
+         * 
+         * 3. work = work + allocation_i
+         *    finish[i] = true.
+         *    Goto step 2.
+         * 
+         * 4. If finish[i] == true for i from 0 to n-1, then the system is in a safe state.
+         */
+
+        Matrix work = this.getAvailable().copy();
+        boolean retVal = false, finish[] = new boolean[this.getN()];
+        int k = 0;
+
+        while ( k < this.getN() ) // Loop until all process have been allocated...
+        {
+            boolean allocated = false;
+
+            for ( int i = 0; i < this.getN(); i++ )
+            {
+                // Checking to see if all needed resources for the ith process can be allocated.
+                if ( ( !finish[i] ) && ( this.check(0, i, work, this.getNeed()) ) )
+                {
+                    // Trying to allocate...
+                    for ( int j = 0; j < this.getM(); j++ )
+                    {
+                        work.getArray()[0][j] = work.getArray()[0][j] - this.getNeed().getArray()[i][j] + this.getMax().getArray()[i][j];
+                    }
+
+                    System.out.println("Allocated process: " + i);
+                    System.out.println("Available resources: ");
+                    work.print(2, 0);
+                    allocated = finish[i] = true;
+                    k++;
+                }
+            }
+
+            if ( !allocated ) // If no allocation, then exit the loop.
+            {
+                break;
+            }
+        }
+
+        if ( k == this.getN() ) // If all processes have been successfully allocated, then we have reached a safe state.
+        {
+            retVal = true;
+        }
+        else
+        {
+            retVal = false;
+        }
+
+        return retVal;
+    }
+
+    public final boolean request(final int process, final Matrix request)
+    {
+        /*
+         * 1. First, check to make sure that the request is less than or equal to need_i.
+         *    If it is, continue. If not, return failure.
+         * 
+         * 2. Next, check to make sure that the request is less than or equal to the available resources.
+         *    If it is, continue. If not, return failure.
+         * 
+         * 3. Pretend to allocate resources to P_i by modifying the state as follows:
+         *    available = available - request.
+         *    allocation_i = allocation_i + request.
+         *    need_i = need_i - request.
+         * 
+         * If this results in a safe state, then the resources are allocated to P_i.
+         * If this results in an unsafe state, then P_i must wait and the previous state is restored.
+         */
+
+        boolean retVal = false;
+
+        if ( this.check(process, 0, this.getNeed(), request) )
+        {
+            if ( this.check(0, 0, this.getAvailable(), request) )
+            {
+                // Make a temporary deep copy of this BankersAlgorithm object for modifications so the current stable state is not lost in the event of an error.
+                BankersAlgorithm temp = this.copy();
+
+                temp.getAvailable().minusEquals(request);
+
+                for ( int j = 0; j < this.getM(); j++ )
+                {
+                    temp.getAllocation().getArray()[process][j] += request.getArray()[0][j];
+                    temp.getNeed().getArray()[process][j] -= request.getArray()[0][j];
+                }
+
+                // Safety check.
+                if ( temp.isSafe() )
+                {
+                    retVal = true;
+                    this.setAvailable(temp.getAvailable());
+                    this.setMax(temp.getMax());
+                    this.setAllocation(temp.getAllocation());
+                }
+            }
+        }
+
+        return retVal;
+    }
+
+    protected final void setAllocation(final Matrix allocation)
+    {
+        this.allocation = allocation;
+    }
+
+    protected final void setAvailable(final Matrix available)
+    {
+        this.available = available;
+    }
+
+    protected final void setM(final int m)
+    {
+        this.m = m;
+    }
+
+    protected final void setMax(final Matrix max)
+    {
+        this.max = max;
+    }
+
+    protected final void setN(final int n)
+    {
+        this.n = n;
+    }
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+*******************************************************************
+
+* Title:     BankersAlgorithm
+* Author:    [Matthew Boyette](mailto:N00868808@ospreys.unf.edu)
+* Class:     Operating Systems (COP 4610)
+* Professor: Dr. Sanjay Ahuja
+* Term:      Fall 2017
+
+*******************************************************************
+
+This is a stand-alone Java project for Operating Systems (COP 4610) to provide deadlock detection and prevention using the Banker's algorithm. It uses the *JAMA* package's *Matrix* data structure, which is included with this repository in a Java archive (**.JAR*). See [*JAMA*'s website](http://math.nist.gov/javanumerics/jama/) for more information.
+
+#File Listing#
+
+Samples:
+
+* ***output_deny_unsafe.java***: Sample input/output for a case denying additional resources because it would result in an unsafe state.
+* ***output_grant_safe.java***: Sample input/output for a case granting additional resources because it would result in an safe state.
+
+Source Code:
+
+* ***BankersAlgorithm.java***: This is where the magic happens.
+
+Support:
+
+* ***README.md***: this file, the program's documentation.
+* ***makefile***: a standard makefile for a Unix-like development environment.
+* ***makefile.windows***: a standard makefile for a Windows development environment. Rename 'makefile' to 'makefile.unix' (or delete it), and then rename 'makefile.windows' to 'makefile'.
+* ***JAMA-1.0.3.jar***: This file contains the *JAMA* package (and most notably, the *Matrix* structure) which is developed by the mathematics department of the **National Institute for Standards and Technology** (*NIST*).
+
+#Compilation#
+
+**NOTE**: The default included makefile assumes a Unix-like environment. In order to compile on Windows, you need to adjust the classpath parameter to be semi-colon delimited (as opposed to colon delimited in Unix-like environments). See the included *makefile.windows* if you don't know what I'm talking about.
+
+* make
+
+#Execution#
+
+**NOTE**: The included makefile assumes a Unix-like environment. In order to execute on Windows, you need to adjust the classpath parameter to be semi-colon delimited (as opposed to colon delimited in Unix-like environments). See the included *makefile.windows* if you don't know what I'm talking about.
+
+* make exec
+
+#Clean Up#
+
+* make clean
